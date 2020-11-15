7f2804c5784d3f8bc13a43f1c42863c5ecfad287
Salah Belkhiria <salahbelkhiria@gmail.com>
2020-11-15 20:43:21 +0100

    first commit

---
+++ Make-it-modular.js
@@ -0,0 +1,38 @@
+var myModule = require('./my-module');
+
+var directory = process.argv[2];
+var filter = process.argv[3];
+
+var callback = function(error, filtered) {
+  if (error) {
+    console.log('error');
+    console.log(error);
+  }
+  for (var index in filtered) {
+    console.log(filtered[index]);
+  }
+};
+
+myModule(directory, filter, callback);
+
+/* my-module.js */
+var fs = require('fs');
+var path = require('path');
+
+function filterFiles(list, filter) {
+  return list.filter(function(file) {
+    return path.extname(file) == '.' + filter;
+  });
+};
+
+module.exports = function(directory, filter, callback) {
+
+  fs.readdir(directory, function(error, list) {
+    if (error) {
+      return callback(error);
+    }
+    var filtered = filterFiles(list, filter);
+    return callback(null, filtered);
+  });
+
+};
